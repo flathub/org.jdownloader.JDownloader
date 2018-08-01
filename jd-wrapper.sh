a9494e9549e3c20d9e1c2addbbe694d994a637fc
Fabian Bornschein <plusfabi@gmail.com>
2018-08-01 23:04:03 +0200

    change standard download path to xdg-download/JDownloder instead of polluting users home directly

--- jd-wrapper.sh
+++ jd-wrapper.sh
@@ -2,13 +2,14 @@
 
 JDDIR=$XDG_DATA_HOME/jdownloader
 JDSETUP=$XDG_CACHE_HOME/JD2Setup.sh
+JDDWNLDFLDR="$(xdg-user-dir DOWNLOAD)/JDownloader"
 
 if [ ! -f $JDDIR/JDownloader.jar ]; then
     install -Dm755 /app/extra/JD2Setup.sh $JDSETUP
     $JDSETUP -q -dir $JDDIR/tmp | zenity --progress --text="Installing JDownloader" --pulsate --no-cancel --auto-close
     mv $JDDIR/tmp/JDownloader.jar $JDDIR
     rm -rf $JDSETUP $JDDIR/tmp
-    zenity --info --text "Download directory: $HOME/JDownloader" --no-wrap --title "JDownloader"
+    zenity --info --text "Download directory: ${JDDWNLDFLDR}" --no-wrap --title "JDownloader"
 fi
 
 java -jar $JDDIR/JDownloader.jar
