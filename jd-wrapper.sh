86927c380010afe85214f6bc23bf2415d297a7d5
Fabian Bornschein <plusfabi@gmail.com>
2018-08-04 23:48:24 +0200

    change the config for the default download folder on app exit from ~/Downloads to XDG-Downloads/JDownloader, Keep access to old files in jdownloader if it was set to ~/JDownloader

--- jd-wrapper.sh
+++ jd-wrapper.sh
@@ -3,6 +3,7 @@
 JDDIR=$XDG_DATA_HOME/jdownloader
 JDSETUP=$XDG_CACHE_HOME/JD2Setup.sh
 JDDWNLDFLDR="$(xdg-user-dir DOWNLOAD)/JDownloader"
+JDGENSETJSON="${XDG_DATA_HOME}/jdownloader/cfg/org.jdownloader.settings.GeneralSettings.json"
 
 if [ ! -f $JDDIR/JDownloader.jar ]; then
     install -Dm755 /app/extra/JD2Setup.sh $JDSETUP
@@ -10,6 +11,12 @@ if [ ! -f $JDDIR/JDownloader.jar ]; then
     mv $JDDIR/tmp/JDownloader.jar $JDDIR
     rm -rf $JDSETUP $JDDIR/tmp
     zenity --info --text "Download directory: ${JDDWNLDFLDR}" --no-wrap --title "JDownloader"
+fi
+
+## Change wrong path ~/Downloads to the new one. 
+if [ -f ${JDGENSETJSON} ]; then
+    sed -i s+\"$(xdg-user-dir DOWNLOAD)\"+\"${JDDWNLDFLDR}\"+g "${JDGENSETJSON}"
+    sed -i s+\"/home/${USER}/Downloads\"+\"${JDDWNLDFLDR}\"+g "${JDGENSETJSON}"
 fi
 
 java -jar $JDDIR/JDownloader.jar
