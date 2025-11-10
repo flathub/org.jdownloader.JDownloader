95e7e7074b0ca725e92a8034748b46657494e558
Emre Çetin <20030146+EmKaCe@users.noreply.github.com>
2025-11-10 15:51:51 +0100

    Update architecture support and replace JD2Setup.sh with JDownloader.jar. Fixes #51

--- jd-wrapper.sh
+++ jd-wrapper.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 JDDIR=${XDG_DATA_HOME}/jdownloader
-JDSETUP=${XDG_CACHE_HOME}/JD2Setup.sh
 
 if [ ! -f ${JDDIR}/JDownloader.jar ]; then
-    install -Dm755 /app/extra/JD2Setup.sh ${JDSETUP}
-    ${JDSETUP} -q -dir ${JDDIR}/tmp | zenity --progress --text="Installing JDownloader" --pulsate --no-cancel --auto-close
-    mv ${JDDIR}/tmp/JDownloader.jar ${JDDIR}
-    rm -rf ${JDSETUP} ${JDDIR}/tmp
+    mkdir -p ${JDDIR}
+    cp /app/extra/JDownloader.jar ${JDDIR}/
 fi
 
 exec java -jar ${JDDIR}/JDownloader.jar "${@}"
