ee77b4e5b5ce91106d9b57c82962d243098d9cb8
Emre Çetin <20030146+EmKaCe@users.noreply.github.com>
2025-11-11 15:32:48 +0100

    Use install instead of copy
    Co-authored-by: Nicolas Le Cam <niko.lecam@gmail.com>

--- jd-wrapper.sh
+++ jd-wrapper.sh
@@ -5,7 +5,7 @@ JDDIR=${XDG_DATA_HOME}/jdownloader
 if ! jar -tvf ${JDDIR}/JDownloader.jar > /dev/null 2>&1; then
     rm -rf ${JDDIR}/JDownloader.jar ${JDDIR}/Core.jar ${JDDIR}/update ${JDDIR}/tmp 2>/dev/null
     mkdir -p ${JDDIR}
-    cp /app/extra/JDownloader.jar ${JDDIR}/
+    install -Dm644 /app/extra/JDownloader.jar ${JDDIR}
 fi
 
 exec java -jar ${JDDIR}/JDownloader.jar "${@}"
