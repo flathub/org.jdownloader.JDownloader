4d0bf60aeb9bb7d6a8611126f6fe87a2484fcd02
Emre Çetin <20030146+EmKaCe@users.noreply.github.com>
2025-11-11 14:46:35 +0100

    Removed redundant file check

--- jd-wrapper.sh
+++ jd-wrapper.sh
@@ -2,7 +2,7 @@
 
 JDDIR=${XDG_DATA_HOME}/jdownloader
 
-if [ ! -f ${JDDIR}/JDownloader.jar ] || ! jar -tvf ${JDDIR}/JDownloader.jar > /dev/null 2>&1; then
+if ! jar -tvf ${JDDIR}/JDownloader.jar > /dev/null 2>&1; then
     rm -f ${JDDIR}/Core.jar
     rm -rf ${JDDIR}/update ${JDDIR}/tmp
     mkdir -p ${JDDIR}
